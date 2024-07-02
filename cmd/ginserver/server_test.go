3162d31e2879ea87493aa5743a43bb6460620f06
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-07-02 19:31:29 +0300

    feat: gin implementation

---
+++ cmd/ginserver/server_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+)
+
+func Test_GetDateTime(t *testing.T) {
+	router := gin.Default()
+	request, _ := http.NewRequest(http.MethodGet, "/datetime", nil)
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response, request)
+	assert.Equal(t, 200, response.Code)
+	assert.Equal(t, strings.Split(time.Now().String(), " ")[0], strings.Split(response.Body.String(), " ")[0])
+	assert.Equal(t, strings.Split(strings.Split(response.Body.String(), " ")[1], ":")[:2], strings.Split(strings.Split(time.Now().String(), " ")[1], ":")[:2])
+
+}
